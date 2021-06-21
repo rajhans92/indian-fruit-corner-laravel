a6d1149faab15d325e9788e4d6dec78eac07e90a
rajhans92 <rupesh.rajhans92@gmail.com>
2021-06-21 16:09:52 +0530

    project setup

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('/', function () {
+    return json_encode(array("status"=>200,"msg"=>"Welcome To Indian fruit corner.","data"=>null));
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
